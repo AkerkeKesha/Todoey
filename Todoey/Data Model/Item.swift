275d80eef605706739f7d36ef50f3f192a6f2fec
AkerkeKesha <akerke.kass@gmail.com>
2019-05-13 15:12:15 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Akerke Balgabekova on 5/13/19.
+//  Copyright © 2019 Akerke Balgabekova. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
